3dc13c0cbba8189639793383354ffa7aa785c028
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2022-11-28 13:36:20 +0100

    added prettier & eslint

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import Icon from './lib/components/utils/icon.svelte';
-  import { IconData } from './lib/enum/iconData';
+  import Icon from './lib/components/utils/icon.svelte'
+  import {IconData} from './lib/enum/iconData'
 </script>
 
 <main>
-  <Icon size={64} iconData={IconData.YOUTUBE} compClass="fill-red-600"/>
+  <Icon size={64} iconData={IconData.YOUTUBE} compClass="fill-red-600" />
 </main>
