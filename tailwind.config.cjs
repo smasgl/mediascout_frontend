ef6f66f9939987a9d718cfa9f76a8e4e6a7ae0cf
Masooglu Simon <simon.masooglu@liebherr.com>
2022-11-28 13:16:42 +0100

    Added app colors to theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+    colors: {
+      primary: "#212121",
+      secondary: "#313131",
+      background: "#101010",
+      accent: "#53aedd",
+      text: "#eeeeee"
+    },
     extend: {},
   },
 
