fa3d2977d3025307146e7ef19f8100ec0bc931ab
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2022-11-28 16:16:55 +0100

    added tabs manager

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import SideMenu from './lib/components/layouts/sideMenu.svelte'
+    import TabsManager from './lib/components/layouts/tabsManager.svelte'
 </script>
 
 <svelte:head>
   <title>MediaScout</title>
 </svelte:head>
 
-<main class="bg-background h-screen w-screen flex flex-col">
+<main class="bg-background h-screen w-screen flex flex-row p-4 space-x-4">
   <SideMenu />
+  <TabsManager />
 </main>
--- src/lib/components/layouts/sideMenu.svelte
+++ src/lib/components/layouts/sideMenu.svelte
@@ -3,8 +3,8 @@
 import Icon from '../utils/icon.svelte'
 
 </script>
-<aside class="w-80 p-4 h-full" aria-label="Sidebar">
-    <div class="p-4 bg-secondary h-full rounded">
+<aside class="w-80 h-full" aria-label="Sidebar">
+    <div class="p-4 bg-primary h-full rounded">
        <a href="." class="flex items-center pl-2 mb-5 space-x-2">
           <Icon iconData={IconData.BINOCULARS} compClass="fill-accent w-8"/>
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MediaScout</span>
