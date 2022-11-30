06552dc43fb82b64c55240bff08ef8e990769ba0
Zino Mahmoud  (LWN) <Mahmoud.Zino@liebherr.com>
2022-11-30 09:11:48 +0100

    added download button to mainview

--- src/lib/components/layouts/mainView.svelte
+++ src/lib/components/layouts/mainView.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import TabsManager from './tabsManager.svelte'
+  import IconedButton from './../utils/iconedButton.svelte';
+  import { IconData } from '../../enum/iconData'
 
   export let userName: string
 </script>
 
 <div class="bg-primary w-full h-full rounded flex flex-col">
-  <span class="text-text self-left text-2xl font-semibold py-2 px-6"
-    >{userName}</span
-  >
+  <div class="flex flex-row justify-between items-center p-4">
+    <span class="text-text self-left text-2xl font-semibold py-2"
+    >{userName}</span>
+    <IconedButton iconData={IconData.DOWNLOAD} compClass="fill-accent h-8 w-8" />
+  </div>
   <TabsManager />
 </div>
